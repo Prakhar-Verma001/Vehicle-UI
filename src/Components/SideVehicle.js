d2f8c25f624a42424f382e402a95a686b79ed1ed
Prakhar Verma <69008001+Prakhar-Verma001@users.noreply.github.com>
2023-12-28 16:25:00 +0530

    Update SideVehicle.js

--- src/Components/SideVehicle.js
+++ src/Components/SideVehicle.js
@@ -11,14 +11,13 @@ import i7 from '../Assests/Images/Icon7.png'
 const SideVehicle = () => {
   return (
     <>
-        <Box pr={1}>
+        <Box px={1}>
             <Stack direction={'column'} spacing={5}>
-                <Typography variant='h1' color={'#0155A5'} fontWeight={700}
-                sx={{pt:1, pl:1}}>
+                <Typography variant='h1' fontSize={'5rem'} color={'#0155A5'} fontWeight={700}>
                     BPS
                 </Typography>
 
-                <Stack direction={'column'} p={1.2} spacing={1}>
+                <Stack direction={'column'} spacing={1} width={1}>
                     <Option src={i1} alt='i1' text='Dashboard'/>
                     <Option src={i2} alt='i2' text='Manage Booking'/>
                     <Option src={i3} alt='i3' text='Manage Vehicle'/>
@@ -28,7 +27,7 @@ const SideVehicle = () => {
                     <Option src={i7} alt='i7' text='Report'/>
                 </Stack>
 
-                <Box p={1.5}>
+                <Box>
                     <Paper sx={{borderRadius:5, bgcolor:'#0155A5', p:2}}>
                         <Stack direction={'column'} spacing={2}>
                             <Typography variant="body1" lineHeight={2} fontWeight={500} fontSize={'0.8rem'} color="white">
@@ -60,16 +59,16 @@ const SideVehicle = () => {
 const Option = (props) => {
   return (
     <>
-        <Button sx={{p:0, textAlign:'left', textTransform:'none'}}>
+        <Button sx={{p:0, textAlign:'left', textTransform:'none', borderRadius:3}}>
             <Link href='#' underline='none' p={0} width={1}>
-                <Stack direction={'row'} alignItems={'center'} spacing={1.5}
-                    pl={2} py={1} borderRadius={3} width={1}
+                <Stack direction={'row'} alignItems={'center'} spacing={1}
+                    borderRadius={3} p={1}
                     sx={{  color:'#464255' ,'&:hover': {
                         bgcolor:'#C5FFF8',
                         color:'#0155A5'
                       }}}>
                     <img src={props.src}  alt={props.alt}/>
-                    <Typography variant='h6' fontWeight={600} fontSize={'1.1rem'}>
+                    <Typography variant='h6' fontWeight={600} lineHeight={1.2} fontSize={'0.98rem'}>
                         {props.text}
                     </Typography>
                 </Stack>
